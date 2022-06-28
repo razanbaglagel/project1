e5912eae8f3ba8c5c4747b48eeafabc53406530c
razanbaglagel <108234638+razanbaglagel@users.noreply.github.com>
2022-06-29 01:14:42 +0300

    Add files via upload

--- sketch_jun29b.ino
+++ sketch_jun29b.ino
@@ -6,18 +6,9 @@ S.attach(A1);
 }
 
 void loop() {
-
-  S.write(180);
-  delay(100);
-  S.write(170);
-  delay(100);
-  S.write(160);
-  delay(100);
-  S.write(150);
-  delay(100);
-  S.write(140);
-  delay(100);
-  S.write(130);
-  delay(100);
+  int Motor=Serial.parseInt();
+  S.write(Motor);
+  Serial.println(Motor);
+ 
  
 }
