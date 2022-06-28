b5ad28c9a5c4dddef0d0f189e245b97519de0ce4
razanbaglagel <108234638+razanbaglagel@users.noreply.github.com>
2022-06-29 01:21:58 +0300

    Add files via upload

---
+++ servomotor2.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+Servo S;
+void setup() {
+  Serial.begin(9600);
+S.attach(A1);
+}
+
+void loop() {
+  
+  S.write(180);
+  delay(100);
+  S.write(170);
+  delay(100);
+  S.write(160);
+  delay(100);
+  S.write(150);
+  delay(100);
+  S.write(140);
+  delay(100);
+  S.write(130);
+  delay(100);
+  
+}
